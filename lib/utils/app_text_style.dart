95e2f0963e49d5d48bf2c3a00ff5358caa5f8b09
Sharifjon <sharifboymuminov@gmail.com>
2024-06-21 15:09:18 +0500

    first commit

---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_map_example/utils/app_colors.dart';
+
+class AppTextStyle {
+  // SeoulNamsan
+  static TextStyle seoulNamsanThin = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w200,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanLight = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w300,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanRegular = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+  static TextStyle seoulNamsanMedium = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanSemiBold = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanBold = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static TextStyle seoulNamsanBlack = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+
+}
